2fbfcc2a40d9a29974c66b7c1142308d78d98c09
Aryan <aryan.ritesh002@gmail.com>
2022-08-03 15:30:16 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import List from "./Pages/List/List";
+import Hotel from "./Pages/Hotel/Hotel";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Home.css";
+import Navbar from '../../Components/Navbar/Navbar';
+import Header from '../../Components/Header/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  )
+}
+
+export default Home
